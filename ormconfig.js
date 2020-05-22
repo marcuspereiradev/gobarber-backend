4d265b814a7fe588db63bbc0a7def3f13c49ce31
Marcus Pereira <marcus.work@hotmail.com>
2020-05-22 16:07:09 -0300

    starting ORM config

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+	"type": "postgres",
+	"host": process.env.TYPEORM_HOST,
+	"port": process.env.TYPEORM_PORT,
+	"username": process.env.TYPEORM_USERNAME,
+	"password": process.env.TYPEORM_PASSWORD,
+	"database": "gostack_gobarber"
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { json } from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
+
 app.use(json());
 app.use(routes);
 
