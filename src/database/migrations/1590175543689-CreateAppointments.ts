de525004fea2f713bb6691bc1a5a04934968e79a
Marcus Pereira <marcus.work@hotmail.com>
2020-05-25 12:39:16 -0300

    Postgres function that generate the uuid

--- src/database/migrations/1590175543689-CreateAppointments.ts
+++ src/database/migrations/1590175543689-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1590175543689
 						type: 'varchar',
 						isPrimary: true,
 						generationStrategy: 'uuid',
+						default: 'uuid_generate_v4()',
 					},
 					{
 						name: 'provider',
